78008a6b60b326ddbf37e492e70f8c03947773e6
Tatiana <tatigerzens@gmail.com>
2023-11-06 15:17:48 -0300

    Update classes and colors for enhanced styling

--- vite-project/src/components/Pelicula.jsx
+++ vite-project/src/components/Pelicula.jsx
@@ -9,14 +9,10 @@ function Pelicula(props) {
         <img src={props.peli.poster} alt="Foto de pelicula" 
              className='imagen-pelicula' />
         <p className="nombre-pelicula">{props.peli.title}</p>
-        <p className="release">{props.peli.releaseDate}</p>
-        <p className="duration">{props.peli.duration}</p>
-        <p className='genres'>{props.peli.genres}</p>
-        <p className='director-pelicula'>{props.peli.director}</p>
-        <p className="actors">{props.peli.mainActors}</p>
+        <p className="release"> {props.peli.releaseDate} |  {props.peli.duration}  |  {props.peli.genres}</p>
+        <p className="score">{props.peli.metascore} {props.peli.rating}</p>
+        <p className='director-pelicula'>{props.peli.director} | {props.peli.mainActors}</p>
         <p className='texto-pelicula'>{props.peli.plot}</p>
-        <p className="score">{props.peli.metascore}</p>
-        <p className="rating">Rating: {props.peli.rating}</p>
       </div>
     </div>
   );
